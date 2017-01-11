897cd9d9bfa10b574f77ba608bcd04a49a9f0222
Walter <relw1wa-github@yahoo.de>
2017-01-11 23:47:54 +0100

    Create AppHeaderComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AuthenticationComponent } from './user/authentication/authentication.component';
-import { SignupComponent } from './user/signup/signup.component';
-import { SigninComponent } from './user/signin/signin.component';
-import { LogoutComponent } from './user/logout/logout.component';
-import { PollsComponent } from './polls/polls/polls.component';
-import { PollDetailComponent } from './polls/poll-detail/poll-detail.component';
-import { PollCreationComponent } from './polls/poll-creation/poll-creation.component';
 import { ErrorComponent } from './errors/error/error.component';
+import { AppHeaderComponent } from './layout/app-header/app-header.component';
+import { LogoutComponent } from './user/logout/logout.component';
+import { PollCreationComponent } from './polls/poll-creation/poll-creation.component';
+import { PollDetailComponent } from './polls/poll-detail/poll-detail.component';
+import { PollsComponent } from './polls/polls/polls.component';
+import { SigninComponent } from './user/signin/signin.component';
+import { SignupComponent } from './user/signup/signup.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    AppHeaderComponent,
     AuthenticationComponent,
-    SignupComponent,
-    SigninComponent,
+    ErrorComponent,
     LogoutComponent,
-    PollsComponent,
-    PollDetailComponent,
     PollCreationComponent,
-    ErrorComponent
+    PollDetailComponent,
+    PollsComponent,
+    SigninComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
